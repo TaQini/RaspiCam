fa9cd828fc7461c85a6fef00859176caa3da7a84
TaQini <742954809@qq.com>
2019-09-13 16:34:46 +0100

    todo pic display

--- RaspiCam/index.py
+++ RaspiCam/index.py
@@ -3,10 +3,19 @@
 from django.shortcuts import render
 from django.views.decorators import csrf
 from time import time, localtime, strftime
+from os import system
+from time import sleep
 
 def index(request):
-    global ctx
     ctx = {}
     if request.POST:
-        ctx['hello'] = request.POST['q']
+        now = int(time())
+        stamp = strftime('%Y%m%d_%H%M%S',localtime(now))
+        path = '/home/pi/RaspiCam/static/'
+        name = 'RaspiCam_'+stamp+'.jpg'
+        cmd = 'raspistill -w 2592 -h 1944 -t 10 -q 100 -o '
+        cmd += path+name
+        system(cmd)
+        sleep(1)
+        ctx['path'] = name
     return render(request, "index.html", ctx)
--- RaspiCam/urls.py
+++ RaspiCam/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.views.static import serve
 from . import index
+
 urlpatterns = [
         url(r'^index$', index.index)
 ]
